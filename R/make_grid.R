5c8c490893c80e0426187aa6abc7b3fa7b3a851e
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-14 11:51:38 -0400

    export in grid

--- R/make_grid.R
+++ R/make_grid.R
@@ -13,14 +13,23 @@ make_grid <- function() {
              sf::st_union()    
   
   canbuf <- sf::st_buffer(canline, dist = 0.5)
-  
+    
   # Cell size 
   cellsize = 0.1
   
   # Make grids
   grid_poly <- sf::st_make_grid(canbuf, cellsize = cellsize)  
-  grid_poly <- grid_poly[canbuf] |>
-               sf::st_sf() |>
+  grid_poly <- grid_poly[canbuf] 
+  grid_poly <- grid_poly[aoi]
+    
+  bbox_poly <- function(bbox, crs) {
+  sf::st_bbox(bbox, crs = sf::st_crs(crs)) |>
+    sf::st_as_sfc()
+  }
+  bb <- bbox_poly(bbox = c(xmin = -180, ymin = 0, xmax = 180, ymax = 75), crs = 4326)
+  grid_poly <- grid_poly[bb]
+
+  grid_poly <- sf::st_sf(grid_poly) |>
                dplyr::mutate(uid = 1:dplyr::n())  
 
   # Raster grid
